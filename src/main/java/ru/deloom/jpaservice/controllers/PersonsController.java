af3f0328789393068fa242a5d7649f0c43ed269e
DeL00M <del00m@mail.ru>
2017-05-05 08:36:48 +0300

    Refactor requests

--- src/main/java/ru/deloom/jpaservice/controllers/PersonsController.java
+++ src/main/java/ru/deloom/jpaservice/controllers/PersonsController.java
@@ -24,35 +24,35 @@ public class PersonsController {
 	@Autowired
 	private PersonsService personsService;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
 	public HttpEntity<List<Persons>> getAllPersons() {
 		List<Persons> personsList = personsService.getAll();
 		return !personsList.isEmpty() ? new ResponseEntity<>(personsList, HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
-	/*@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public HttpEntity<List<Persons>> getByName(@RequestParam(value = "name", required = true) String name) {
+	@RequestMapping(value = "/name/{name}", method = RequestMethod.GET, produces = "application/json")
+	public HttpEntity<List<Persons>> getByName(@PathVariable("name") String name) {
 		List<Persons> personsList = personsService.getByName(name);
 		return !personsList.isEmpty() ? new ResponseEntity<>(personsList, HttpStatus.OK)
 				: new ResponseEntity<>(personsList, HttpStatus.NOT_FOUND);
-	}*/
+	}
 	
-	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public HttpEntity<Persons> getById(@RequestParam(value = "id", required = true) Long id) {
+	@RequestMapping(value = "/id/{id}",method = RequestMethod.GET, produces = "application/json")
+	public HttpEntity<Persons> getById(@PathVariable("id") Long id) {
 		Persons persons = personsService.getById(id);
 		return persons != null ? new ResponseEntity<>(persons, HttpStatus.OK)
 				: new ResponseEntity<>(persons, HttpStatus.NOT_FOUND);
 	}
 
-	@RequestMapping(value = "/{name}", method = RequestMethod.POST)
+	@RequestMapping(value = "/add/{name}", method = RequestMethod.POST)
 	public HttpEntity<Boolean> add(@PathVariable("name") String name) {
 		return personsService.addPerson(name) ? new ResponseEntity<>(true, HttpStatus.CREATED)
 				: new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
-	public HttpEntity<Boolean> update(@RequestParam("id") Long id, @RequestParam("name") String name) {
+	@RequestMapping(value = "/edit/id/{id}/{name}", method = RequestMethod.PUT)
+	public HttpEntity<Boolean> update(@PathVariable("id") Long id, @PathVariable("name") String name) {
 		return personsService.editPerson(id, name) ? new ResponseEntity<>(true, HttpStatus.OK)
 				: new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
 	}
