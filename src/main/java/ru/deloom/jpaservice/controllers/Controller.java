fb21814d594638f95918046bdf2f91f30d3f5d45
DeL00M <del00m@mail.ru>
2017-05-05 17:30:21 +0300

    refactoring in progress

--- src/main/java/ru/deloom/jpaservice/controllers/Controller.java
+++ src/main/java/ru/deloom/jpaservice/controllers/Controller.java
@@ -2,7 +2,6 @@ package ru.deloom.jpaservice.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +13,6 @@ import ru.deloom.jpaservice.services.Service;
 
 public class Controller {
 
-	//@Autowired
 	public Service service;
 
 	public Controller(Service service) {
