b34321abbaa27487758a8809f47162f5761cb01b
Del00M <del00m@mail.ru>
2017-05-05 21:30:06 +0300

    fix

--- src/main/java/ru/deloom/jpaservice/services/SitesService.java
+++ src/main/java/ru/deloom/jpaservice/services/SitesService.java
@@ -1,15 +1,5 @@
 package ru.deloom.jpaservice.services;
 
-import java.util.List;
+public interface SitesService extends Service{
 
-import ru.deloom.jpaservice.entities.Sites;
-
-public interface SitesService {
-	Boolean add(String name);
-	Boolean exist(Long id);
-	void delete(Long id);
-	Boolean edit(Long id, String newname);
-	List<Sites> getByName(String name);
-	List<Sites> getAll();
-	Sites getById(Long id);
 }
